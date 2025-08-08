8b153047163c608d47d2b1d3de10b7533ead6520
Yining-anna-tang <annatang@Stanford.edu>
2025-08-08 16:07:04 +0800

    Update 11.Wind diagram.py

--- 11.Wind diagram.py
+++ 11.Wind diagram.py
@@ -4,29 +4,28 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# 生成随机的风速和风向数据
-# 1.读取数据，设置变量
-data = pd.read_csv(r'/Users/yiningtang/PycharmProjects/pythonProject1/WTP.csv',encoding = "GBK")
+# Read data and set variables
+data = pd.read_csv(r'/Users/yiningtang/PycharmProjects/pythonProject1/WTP.csv', encoding="GBK")
 df = pd.DataFrame(data)
 wd = df['Y']
 ws = df['age']
 
-# 创建风玫瑰图对象，并绘制归一化柱状图
+# Create windrose plot object and plot normalized bar chart
 ax = WindroseAxes.from_ax()
-ax.bar(wd, ws, normed=True, opening=0.8, edgecolor="white")  # 绘制柱状图
-ax.set_legend()  # 添加图例
+ax.bar(wd, ws, normed=True, opening=0.8, edgecolor="white")
+ax.set_legend()
 plt.show()
 plt.figure()
 
-# 创建风玫瑰图对象，并绘制箱线图
+# Create windrose plot object and plot boxplot
 ax = WindroseAxes.from_ax()
-ax.box(wd, ws, bins=np.arange(0, 8, 1))  # 绘制箱线图
-ax.set_legend()  # 添加图例
+ax.box(wd, ws, bins=np.arange(0, 8, 1))
+ax.set_legend()
 plt.show()
 plt.figure()
 
-# 创建风玫瑰图对象，并绘制填充等高线图
+# Create windrose plot object and plot filled contour plot
 ax = WindroseAxes.from_ax()
-ax.contourf(wd, ws, bins=np.arange(0, 8, 1), cmap=cm.hot)  # 绘制填充等高线图
-ax.set_legend()  # 添加图例
+ax.contourf(wd, ws, bins=np.arange(0, 8, 1), cmap=cm.hot)
+ax.set_legend()
 plt.show()
