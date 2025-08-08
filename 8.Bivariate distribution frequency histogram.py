20723cfa1d25ecbe0a212fd1fec84a68a93c9728
Yining-anna-tang <annatang@Stanford.edu>
2025-08-08 13:51:13 +0800

    Update 8.Bivariate distribution frequency histogram.py

--- 8.Bivariate distribution frequency histogram.py
+++ 8.Bivariate distribution frequency histogram.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-sns.set(font='Times New Roman', font_scale=0.8, style="darkgrid") # 解决Seaborn中文显示问题
+sns.set(font='Times New Roman', font_scale=0.8, style="darkgrid")
 
-# 导入数据
-# 1.读取数据，设置变量
-data = pd.read_csv(r'/Users/yiningtang/PycharmProjects/pythonProject1/WTP.csv',encoding = "GBK")
+data = pd.read_csv(r'/Users/yiningtang/PycharmProjects/pythonProject1/WTP.csv', encoding="GBK")
 df = pd.DataFrame(data)
 
-sns.histplot(data=df, x="Y", color="skyblue", label="Y", kde=True)
-sns.histplot(data=df, x="age", color="red", label="age", kde=True)
+sns.histplot(data=df, x="Y", color="skyblue", label="Willingness to Pay", kde=True)
+sns.histplot(data=df, x="age", color="red", label="Age", kde=True)
 
 plt.legend()
+plt.xlabel("Value")
+plt.ylabel("Density")
+plt.title("Distribution of Willingness to Pay and Age")
+plt.tight_layout()
 plt.show()
